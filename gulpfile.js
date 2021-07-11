72bcd4f40c9eaa9aa854d635a6963455c8b157c2
Tom Marks <tom@tommarks.xyz>
2021-07-11 15:15:17 +1000

    Added webserver to gulp, refactor some constants into gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,32 @@
 const gulp = require("gulp");
 const wp = require("webpack-stream");
 // const nd = require("gulp-nodemon");
+const ws = require("gulp-webserver");
+
+const WP_CONFIG = "./webpack.config.js";
+const SRC = "./src/*.js";
+const DEST = "./http/js";
 
 function clientBuild() {
-    return gulp.src(["src/*.js"])
-        .pipe(wp(require("./webpack.config.js")))
-        .pipe(gulp.dest("http/js"));
+    return gulp.src([SRC])
+        .pipe(wp(require(WP_CONFIG)))
+        .pipe(gulp.dest(DEST));
+}
+
+function webserver() {
+    return gulp.src("http").pipe(ws({
+        host: "localhost",
+        port: 8000,
+        livereload: true,
+        open: true,
+        fallback: "./http/index.js"
+    }));
 }
 
 function watchClientBuild() {
     clientBuild();
-    return gulp.watch(["src/*.js"], clientBuild);
+    webserver();
+    return gulp.watch([SRC], clientBuild);
 }
 
 exports.clientBuild = clientBuild;
